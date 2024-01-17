bb8f9e5c968571ed66f90dac162a84137587ff59
Prince <princeeze4302@gmail.com>
2024-01-17 14:53:11 +0000

    grid now generates unique colours

--- gui.gd
+++ gui.gd
@@ -3,13 +3,31 @@ extends Control
 @onready var slot_scene = preload("res://slot.tscn")
 @onready var palette_grid = $Grid/PaletteGrid
 
+const COLOURS_TO_MIX = 2
 var grid_array := []
+var colour_depth = 3
+var colour_array = []
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	for i in range(8):
-		create_slot()
+	var r = 0.0
+	var g = 0.0
+	var b = 0.0
+	var current_colour = [-1,-1,-1]
+	while colour_array.size() < 9:
+		r = (1.0 / (colour_depth-1)) * (randi_range(0,colour_depth-1))
+		g = (1.0 / (colour_depth-1)) * (randi_range(0,colour_depth-1))
+		b = (1.0 / (colour_depth-1)) * (randi_range(0,colour_depth-1))
+		current_colour = [r,g,b]
+		if current_colour not in colour_array:
+			colour_array.push_back(current_colour)
+		current_colour = [-1,-1,-1]
 
+	for i in range(9):
+		create_slot()
+	for i in range(9):
+		grid_array[i].set_background(colour_array[i])
+		#print(grid_array[i].get_background(), colour_array[i], "\n")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -20,3 +38,5 @@ func create_slot():
 	new_slot.slot_ID = grid_array.size()
 	palette_grid.add_child(new_slot)
 	grid_array.push_back(new_slot)
+	
+	
--- slot.gd
+++ slot.gd
@@ -1,6 +1,7 @@
 extends ColorRect
 
 var slot_ID := -1
+var colour_data = Color(1.0,1.0,1.0)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -10,3 +11,8 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+func set_background(c):
+		color = Color(c[0],c[1],c[2])
+func get_background():
+	return color
