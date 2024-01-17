82a054e5dd1510b94feb3ecf459367eaf7c2ad6d
Prince <princeeze4302@gmail.com>
2024-01-17 21:33:54 +0000

    score and highscore is now functional and difficulties are coded

--- Swatch.gd
+++ Swatch.gd
@@ -13,10 +13,18 @@ func _process(delta):
 	pass
 
 func set_background(c):
-		color = Color(c[0],c[1],c[2])
+	color = Color(c[0],c[1],c[2])
 func get_background():
 	return color
+func set_swatch_size(s):
+	custom_minimum_size = Vector2(s,s)
 
+
+
+
+func get_swatch_size():
+	return [size.x, size.y]
+	
 func _on_swatch_gui_input(event):
 	if event.is_action_pressed("mouse_left"):
 		emit_signal("swatch_selected", self)
--- gui.gd
+++ gui.gd
@@ -2,13 +2,22 @@ extends Control
 
 @onready var swatch_scene = preload("res://Swatch.tscn")
 @onready var palette_grid = $Palette/PaletteGrid
+@onready var score_node = $Score
+@onready var highscore_node = $HighScore
 
 const COLOURS_TO_MIX = 2
+var COLOUR_DEPTH = 3
+
+
+var selected_difficulty := 0
+var PALETTE_SIZE = 4
+var SWATCH_SIZE = 176
+
 var grid_array := []
-var colour_depth = 3
 var colour_array = []
 var correct_swatches = [null, null]
 var score = 0
+var highscore = 0
 var swatches_selected = [null, null]
 
 func new_palette():
@@ -17,12 +26,12 @@ func new_palette():
 	var b = 0.0
 	var current_colour = [-1,-1,-1]
 	var x = 0
-	while x < 9:
-		if colour_depth <=2:
-			colour_depth = 3
-		r = (1.0 / (colour_depth-1)) * (randi_range(0,colour_depth-1))
-		g = (1.0 / (colour_depth-1)) * (randi_range(0,colour_depth-1))
-		b = (1.0 / (colour_depth-1)) * (randi_range(0,colour_depth-1))
+	while x < PALETTE_SIZE:
+		if COLOUR_DEPTH <=2:
+			COLOUR_DEPTH = 3
+		r = (1.0 / (COLOUR_DEPTH-1)) * (randi_range(0,COLOUR_DEPTH-1))
+		g = (1.0 / (COLOUR_DEPTH-1)) * (randi_range(0,COLOUR_DEPTH-1))
+		b = (1.0 / (COLOUR_DEPTH-1)) * (randi_range(0,COLOUR_DEPTH-1))
 		current_colour = [r,g,b]
 		if current_colour not in colour_array:
 			#print(colour_array)
@@ -32,9 +41,17 @@ func new_palette():
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	colour_array.resize(9)
+	if selected_difficulty == 0:
+		PALETTE_SIZE = 4
+		SWATCH_SIZE = 174
+	else:
+		PALETTE_SIZE = 9
+		SWATCH_SIZE = 116
+	palette_grid.columns = sqrt(PALETTE_SIZE)
+	
+	colour_array.resize(PALETTE_SIZE)
 	colour_array.fill(null)
-	grid_array.resize(9)
+	grid_array.resize(PALETTE_SIZE)
 	grid_array.fill(null)
 	new_palette()
 	create_palette()
@@ -46,15 +63,17 @@ func _process(delta):
 
 		
 func create_palette():
-	for i in range(9):
+	for i in range(PALETTE_SIZE):
 		var new_swatch = swatch_scene.instantiate()
 		new_swatch.swatch_ID = i
+		new_swatch.set_swatch_size(SWATCH_SIZE)
 		#print(colour_array)
 		#print(new_swatch.swatch_ID)
 		new_swatch.set_background(colour_array[new_swatch.swatch_ID])
 		new_swatch.swatch_selected.connect(_on_swatch_selected)
 		palette_grid.add_child(new_swatch)
 		grid_array[i] = new_swatch
+	print(grid_array[0].get_background)
 	
 func _on_swatch_selected(swatch):
 	print(swatch.swatch_ID)
@@ -71,7 +90,7 @@ func _on_swatch_selected(swatch):
 func reset_palette():
 	new_palette()
 	swatches_selected = [null, null]
-	for i in range(9):
+	for i in range(PALETTE_SIZE):
 		grid_array[i].set_background(colour_array[grid_array[i].swatch_ID])
 	create_result()
 
@@ -82,17 +101,17 @@ func check_correct():
 			score+=1
 		else:
 			print("incorrect")
+			if score>highscore: highscore=score
 			score=0
 		reset_palette()
-		print("Score is: ",score)
+		score_node.text = str(score)
+		highscore_node.text = str(highscore)
 		
 func create_result():
 	var current_rand = -1
-	correct_swatches[0] = randi_range(0,8)
-	while correct_swatches[1] == null:
-		current_rand = randi_range(0,8)
-		if current_rand not in correct_swatches:
-			correct_swatches[1] = current_rand
+	correct_swatches = [randi_range(0,PALETTE_SIZE-1),randi_range(0,PALETTE_SIZE-1)]
+	while correct_swatches[1] == correct_swatches[0]:
+		correct_swatches[1] = randi_range(0,PALETTE_SIZE-1)
 	
 	var new_r = (grid_array[correct_swatches[0]].get_background().r
 	+ grid_array[correct_swatches[1]].get_background().r) / 2
